fb483a87d824516ad47350b84d4d10d11d20bbb2
Mason France <masonrf1@gmail.com>
2021-09-01 17:54:15 -0600

    Embed twitter feed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,34 @@
 import React, { useState } from 'react';
 import '../css/home.css';
+import TwitterTimelineEmbed from 'react-tweet-embed'
 
 function Home(props) {
   return (
     <div className='home'>
       <h2 className='bmarg'>Welcome!</h2>
       <h3 className='bmarg'>We are very excited to host you for our wedding!</h3>
+      <h3 className='bmarg'>Please check out the twitter feed below for live updates about our whereabouts.</h3>
       <p className='bmarg'>Please look through this page using the navigation buttons above to find information about our wedding on September 5th at Hudson Gardens.</p>
-      <p>A few important things to note as you look at the website:</p>
-      <h3>RSVPs are needed asap for Monday's brunch!</h3>
-      <h3 className='bmarg'>The FAQs are worth checking out!</h3>
-      <p className='bmarg'>Email us at CelebrateMasAndSam@gmail.com with any questions and to RSVP for brunch.</p>
+      <h3 className='bmarg'>The FAQs page is worth checking out!</h3>
+      <p className='bmarg'>Email us at CelebrateMasAndSam@gmail.com with any questions.</p>
+      <a class="twitter-timeline" href="https://twitter.com/CelebrateMasSam?ref_src=twsrc%5Etfw">Tweets by CelebrateMasSam</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+      <section className="twitterContainer">
+      <div className="twitter-embed">
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="CelebrateMasSam"
+          options={{
+            tweetLimit: "10",
+            width: "100%",
+            height: "100%"
+          }}
+          theme="dark"
+          noHeader="true"
+          noBorders="true"
+          noFooter="true"
+        ></TwitterTimelineEmbed>
+      </div>
+    </section>
     </div>
   );
 }
--- src/Components/Schedule.js
+++ src/Components/Schedule.js
@@ -12,12 +12,11 @@ function Schedule(props) {
         <h4><a href='https://www.hudsongardens.org/' target='_blank'>Hudson Gardens</a> - <a href='https://www.google.com/maps/place/The+Hudson+Gardens+%26+Event+Center/@39.6065212,-105.0219713,14.83z/data=!4m5!3m4!1s0x876c81d1cec00685:0x4bd927d7813c89de!8m2!3d39.6047036!4d-105.0220777'>Littleton, CO</a></h4>
         <h4>6pm - Reception to follow</h4>
       </header>
-      <h3><i>Check back for updates!</i></h3>
       <article className='days'>
         <div className='day'>
           <h3>Friday</h3>
-          <p>Breweries and Downtown</p>
-          <p>Great Divide, Woods Boss, 5 Points neighborhood are possibilities</p>
+          <p>Breweries Downtown</p>
+          <p>Woods Boss, then Great Divide, then ??? (stay tuned to Twitter feed!)</p>
         </div>
         <div className='day'>
           <h3>Saturday</h3>
@@ -29,7 +28,7 @@ function Schedule(props) {
           <h3>Sunday</h3>
           <p>6pm Wedding at Hudson Gardens</p>
           <p>Dinner and dancing to follow</p>
-          <p>11pm to 2am after party at <a href='https://coloradopinballpub.com/' target='_blank'>Colorado Pinball Pub</a></p>
+          <p>10:30pm to 2am after party at <a href='https://coloradopinballpub.com/' target='_blank'>Colorado Pinball Pub</a></p>
         </div>
         <div className='day'>
           <h3>Monday</h3>
@@ -40,9 +39,9 @@ function Schedule(props) {
       <article className='activities'>
         <h3>Other Activities</h3>
         <h4>This will be a fun time to visit Denver!</h4>
+        <h4><a href='https://www.9news.com/article/entertainment/events/9-things-to-do/things-to-do-colorado-sept-3/73-e79a16f5-ea6d-4733-9dbc-e0ffa648f1f9?fbclid=IwAR1LiEQP57ztwStwB7TW0S1H9vb5ehAIy_aYodHxl6WU6li1-Es_dI2iZqQ' target='_blank'>Guide of labor day activities.</a></h4>
         <p>Spend time downtown for <a href='https://www.facebook.com/ATasteofColorado/' target='_blank'>Taste of Colorado</a>.</p>
         <p>Catch a <a href='https://www.mlb.com/rockies/tickets/single-game-tickets?affiliateId=tdl-Colorado_Rockies-tickets-Colorado_Rockies%3A_Tickets%3A_Rockies_Tickets-na-x0-Phone-Portrait&fbclid=IwAR1vDQcV11G_p90RNbIk-IqPfPgiHprVhCzYh5qhtTlhcz8_AvyFMyDg2wk' target='_blank'>Rockies game</a>.</p>
-        <p><a href='https://www.hudsongardens.org/enjoy/beer-festival/' target='_blank'>TBD beer fest at Hudson Gardens</a>? Check back here for updates.</p>
         <p>Denver has any number of fun activities - Get in touch for any reccomendations!</p>
         <p><i>Email us! CelebrateMasAndSam@gmail.com</i></p>
       </article>
